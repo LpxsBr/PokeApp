e4dbf203adc0bd1c9e7d491210b65e3e44b9c45e
LpxsBr <anselmolopes.an@gmail.com>
2023-02-09 17:52:37 -0300

    feat: add navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <Navbar/>
   <RouterView />
 </template>
 
@@ -30,6 +12,10 @@ import HelloWorld from "./components/HelloWorld.vue";
 header {
   line-height: 1.5;
   max-height: 100vh;
+}
+
+.router{
+  margin-top: 100px;
 }
 
 .logo {
@@ -62,30 +48,4 @@ nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <header>
+        <nav>
+            <div class="logo">
+                <img src="https://uploads-ssl.webflow.com/6356e75722f218bac247e7c6/636180db578c477db8820169_logo-branco.svg"/>
+            </div>
+            <div class="menu">
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style>
+*{
+    margin: 0%;
+}
+header{
+    width: 100%;
+    height: 80px;
+    background: purple;
+    display: flex;
+    border: 1px solid white;
+    justify-content: center;
+    align-items: center;
+}
+a{
+    text-decoration: none;
+    color: #fff;
+    margin: 10px;
+}
+a:visited{
+    color: #fff;
+}
+nav{
+    width: 100%;
+    display: flex;
+    gap: 70%;
+    justify-content: center;
+    align-items: center;
+}
+</style>
