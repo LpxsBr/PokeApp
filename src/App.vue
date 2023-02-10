afc514f2fb0d119086f3c3d87f58dce13d193141
LpxsBr <anselmolopes.an@gmail.com>
2023-02-09 22:43:02 -0300

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import Footer from "./components/Footer.vue";
 import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
   <Navbar/>
   <RouterView />
+  <Footer />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.router{
-  margin-top: 100px;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
 
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import IconGitHub from './icons/IconGitHub.vue';
+</script>
+
+<template>
+    <footer>
+        <div class="item">
+            <div class="logo">
+                <img class="copy-logo" alt="logo-copybase" src="https://uploads-ssl.webflow.com/6356e75722f218bac247e7c6/636180db578c477db8820169_logo-branco.svg"/>
+                <img class="pokeapi-logo" alt="logo-pokeapi" src="https://avatars.githubusercontent.com/u/64151210?v=4"/>
+            </div>
+            <div class="menu">
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </div>
+            <div class="social">
+                <IconGitHub/>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<style>
+*{
+    /* margin: 0%; */
+}
+footer{
+    width: 100%;
+    height: 200px;
+    background: purple;
+    display: flex;
+    border: 1px solid white;
+    justify-content: center;
+    align-items: center;
+    gap: 10%;
+}
+a{
+    text-decoration: none;
+    color: #fff;
+    margin: 10px;
+}
+a:visited{
+    color: #fff;
+}
+footer 
+.menu{
+    display: flex;
+    flex-direction: column;
+}
+
+.logo{
+    width: calc(100% / 3);
+    display: flex;
+    gap: 5%;
+}
+.pokeapi-logo{
+    width: 30vh;
+}
+.social{
+    /* width: calc(100% / 3); */
+    display: flex;
+    gap: 10%;
+}
+.item{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 60%;
+}
+</style>
