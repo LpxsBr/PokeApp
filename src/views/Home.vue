4e582c528578b26aa41adaf952cac10814fa2269
LpxsBr <anselmolopes.an@gmail.com>
2023-02-09 22:43:40 -0300

    feat: add style and components in home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,46 @@
+
+<script setup>
+  import Search from '../components/Search.vue';
+  const test = () => {
+    console.log("aiaiaiai")
+    document.getElementById("test").innerHTML = "Pesquisou"
+  }
+</script>
+
 <template>
-    <div>
-        <h1>Home</h1>
-    </div>
+    <main>
+        <div class="container">
+            <div class="pokeapi-main-logo">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/800px-International_Pok%C3%A9mon_logo.svg.png" alt="pokeapi-logo">
+            </div>
+            <Search :btn="true" :btnPlaceholder="'pesquisar'" :search="()=>test()" />
+        </div>
+    </main>
 </template>
 
 <style>
 *{
     color: #000;
 }
+
+main{
+    height: calc(100vh - (200px + 80px));
+}
+.pokeapi-main-logo{
+    display: flex;
+    justify-content: center;
+}
+img{
+    width: 40%;
+}
+.container{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: -40%;
+}
+
+
+
 </style>
