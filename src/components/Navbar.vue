0debeb31246d56ad704421f60e99d439159747a5
LpxsBr <anselmolopes.an@gmail.com>
2023-02-11 13:45:06 -0300

    style: adjust

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,7 +6,7 @@ import { RouterLink } from 'vue-router';
     <header>
         <nav>
             <div class="logo">
-                <img src="https://uploads-ssl.webflow.com/6356e75722f218bac247e7c6/636180db578c477db8820169_logo-branco.svg"/>
+                <img class="copybase" src="https://uploads-ssl.webflow.com/6356e75722f218bac247e7c6/636180db578c477db8820169_logo-branco.svg"/>
             </div>
             <div class="menu">
                 <RouterLink to="/">Home</RouterLink>
@@ -17,7 +17,39 @@ import { RouterLink } from 'vue-router';
 </template>
 
 <style>
-*{
+
+header{
+    width: 100%;
+    background-color: rgb(0, 0, 143);
+}
+nav{
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+nav .logo{
+    display: flex;
+    width: 50%;
+    justify-content: center;
+}
+
+nav .logo .copybase{
+    width: 100px;
+}
+
+nav .menu{
+    display: flex;
+    width: 50%;
+    justify-content: center;
+}
+@media (max-width: 600px){
+    nav{
+        flex-direction: column;
+    }
+}
+/* *{
     margin: 0%;
 }
 header{
@@ -40,8 +72,23 @@ a:visited{
 nav{
     width: 100%;
     display: flex;
-    gap: 70%;
+    gap: 40%;
     justify-content: center;
     align-items: center;
 }
+
+nav.logo{
+    width: 100%;
+}
+
+@media (max-width: 500px){
+    nav{
+        width: 100%;
+        display: flex;
+        gap: 3%;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+} */
 </style>
